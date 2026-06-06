23b3cbdcc7ed98c755b2d464b9723eb908549f71
Zen-333 <zenhamam56@gmail.com>
2026-06-06 23:08:12 +0100

    missile class init

---
+++ ArcadeApp/ArcadeApp/Games/Asteroids/Missile.cpp
@@ -0,0 +1,36 @@
+#include "Missile.h"
+
+Missile::Missile()
+{
+
+}
+
+void Missile::Init()
+{
+
+}
+
+void Missile::Update(uint32_t dt)
+{
+
+}
+
+void Missile::Launch(Vec2D& LaunchPoint)
+{
+
+}
+
+void Missile::Draw(Screen& theScreen)
+{
+
+}
+
+void Missile::Deactivate()
+{
+
+}
+
+void Missile::Activate()
+{
+
+}
--- ArcadeApp/ArcadeApp/Games/Asteroids/Missile.h
+++ ArcadeApp/ArcadeApp/Games/Asteroids/Missile.h
@@ -1,1 +1,30 @@
 #pragma once
+
+#include "Vec2D.h"
+#include "SpriteSheet.h"
+#include "AnimatedSprite.h"
+
+class Missile {
+
+public:
+
+	Missile();
+	void Init();
+	void Update(uint32_t dt);
+	void Launch(Vec2D& LaunchPoint);
+	void Draw(Screen& theScreen);
+
+private:
+
+	void Deactivate();
+	void Activate();
+
+	const float mSpeed = 5;
+	bool mActive = false;
+
+	Vec2D mVelocity = Vec2D::Zero;
+	SpriteSheet mSpriteSheet;
+	AnimatedSprite mSprite;
+	AnimatedSprite mThruster;
+	Vec2D mLaunchPoint;
+};
